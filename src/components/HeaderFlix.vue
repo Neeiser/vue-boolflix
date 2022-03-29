65213fbab71776fa6ef2a7872eb9efb360a963e7
Neeiser <stortini.study@gmail.com>
2022-03-29 11:44:39 +0200

    Some basic structuring to start with

---
+++ src/components/HeaderFlix.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+      <div>
+          <h1>BOOLFLIX</h1>
+      </div>
+      <div>
+          <input type="text" 
+          placeholder="Inserisci il nome:" name="searchInput" id="searchInput"
+          value="">
+          <button>Cerca</button>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name:'HeaderFlix',
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    background-color: lightgray;
+}
+</style>
+
