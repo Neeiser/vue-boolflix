f24c004aaca1aca3438481c2ef647fc901b16780
Neeiser <stortini.study@gmail.com>
2022-03-29 15:03:43 +0200

    more structure for the user input

--- src/components/HeaderFlix.vue
+++ src/components/HeaderFlix.vue
@@ -6,15 +6,26 @@
       <div>
           <input type="text" 
           placeholder="Inserisci il nome:" name="searchInput" id="searchInput"
-          value="">
+          value="" v-model="textUserInput">
           <button>Cerca</button>
       </div>
   </header>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name:'HeaderFlix',
+    data(){
+        return{
+            textUserInput: '',
+        };
+    },
+    created(){
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&language=it-IT')
+        .then
+    }
 }
 </script>
 
