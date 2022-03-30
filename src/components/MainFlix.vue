b27adc708f1e451f34a8a80a97d61b8e8841023f
Neeiser <stortini.study@gmail.com>
2022-03-30 15:28:54 +0200

    v-for for the films into the main

--- src/components/MainFlix.vue
+++ src/components/MainFlix.vue
@@ -1,6 +1,10 @@
 <template>
     <main>
-
+        <div class="card"
+        v-for="(film, index) in arrFilms" 
+        :key="index">
+            <h2>{{film.title}}</h2>
+        </div>
     </main>
 </template>
 
@@ -8,11 +12,15 @@
 export default {
     name:'MainFlix',
     props:{
-    films: Array,
+    arrFilms: Object,
   },
 }
 </script>
 
 <style lang="scss" scoped>
+
+.card{
+    background-color: lightblue;
+}
 
 </style>>
