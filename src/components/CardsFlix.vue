2bd120a00ea337b6c90a8270beefab51be7056b1
Neeiser <stortini.study@gmail.com>
2022-03-30 23:41:26 +0200

    Imported lang flags with an Api - does not work with en language

--- src/components/CardsFlix.vue
+++ src/components/CardsFlix.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="card">
-        <img :src="imgPath" alt="">
+        <img id="imgPoster" :src="imgPath" alt="">
         <!-- <img :src="backupImgPath" alt=""> --> <!-- Mi serve per dopo -->
         <h2>{{filmElement.title}}</h2>
         <h3>{{filmElement.original_title}}</h3>
-        <small>{{filmElement.original_language}}</small>
+        <img id="flagPoster" :src="langFlag">
         <small>{{filmElement.vote_average}}</small>
     </div>
 </template>
@@ -14,6 +14,7 @@ export default {
     name:'CardsFlix',
     data(){
         return{
+            langFlag: 'https://countryflagsapi.com/png/' + this.filmElement.original_language,
             imgPath: 'https://image.tmdb.org/t/p/w500' + this.filmElement.poster_path,
             /* backupImgPath: 'https://image.tmdb.org/t/p/w500' + this.filmElement.backdrop_path */  /* Mi serve per dopo */
         }
@@ -34,8 +35,11 @@ export default {
     align-items: center;
     height: 600px;
     width: 300px;
-    img{
+    #imgPoster{
         width: 200px;
+    }
+    #flagPoster{
+        width: 50px;
     }
 }
 
