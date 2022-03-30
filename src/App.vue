f365b68cb81a092d3df3fec3d994e2a0a261188d
Neeiser <stortini.study@gmail.com>
2022-03-30 15:23:49 +0200

    Main import

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
     <HeaderFlix @search="searchFunction"></HeaderFlix>
+    <MainFlix></MainFlix>
   </div>
 </template>
 
 <script>
 import HeaderFlix from './components/HeaderFlix.vue'
+import MainFlix from './components/MainFlix.vue'
 import axios from 'axios';
 
 export default {
   name: 'App',
   data(){
-        return{
-            textUserInput: '',
-        };
-    },
+    return{
+      arrResponse: [],
+    };
+  },
   components:{
     HeaderFlix,
+    MainFlix,
   },
   methods:{
     searchFunction(argText){
         axios.get('https://api.themoviedb.org/3/search/movie?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&language=it-IT&query=' + argText)
         .then((item) =>{
-            this.arrResponse = item.data.results; /* + [this.resultCycle] */
+            this.arrResponse = item.data.results;
             console.log(this.arrResponse);
         })
     }
--- src/components/MainFlix.vue
+++ src/components/MainFlix.vue
@@ -7,6 +7,9 @@
 <script>
 export default {
     name:'MainFlix',
+    props:{
+    films: Array,
+  },
 }
 </script>
 
