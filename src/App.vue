7cf5dbdcded6c3ff7dd00bc68e09fa7237bba71d
Neeiser <stortini.study@gmail.com>
2022-03-30 11:25:12 +0200

    back to the start

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,17 @@
 <template>
   <div id="app">
-    <HeaderFlix @searchTitle="startSearchFunction"></HeaderFlix>
-    <MainFlix></MainFlix>
+    <HeaderFlix></HeaderFlix>
   </div>
 </template>
 
 <script>
 import HeaderFlix from './components/HeaderFlix.vue'
-import MainFlix from './components/MainFlix.vue'
-import axios from 'axios';
 
 export default {
   name: 'App',
   components:{
     HeaderFlix,
-    MainFlix,
   },
-  data(){
-      return{
-          arrResponse: [],
-          resultCycle: 0,
-      };
-  },
-  methods:{
-      startSearchFunction(){
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&language=it-IT&query=' + this.textUserInput)
-        .then((item) =>{
-            this.arrResponse = item.data.results; /* + [this.resultCycle] */
-            console.log(this.arrResponse);
-        })
-      }
-  }
 }
 </script>
 
