04450f5f55c45403b06af917238c4d80decc828f
Neeiser <stortini.study@gmail.com>
2022-03-30 11:13:55 +0200

    methods search moved into App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <HeaderFlix></HeaderFlix>
+    <HeaderFlix @searchTitle="startSearchFunction"></HeaderFlix>
+    <MainFlix></MainFlix>
   </div>
 </template>
 
 <script>
 import HeaderFlix from './components/HeaderFlix.vue'
+import MainFlix from './components/MainFlix.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components:{
     HeaderFlix,
+    MainFlix,
   },
+  data(){
+      return{
+          arrResponse: [],
+          resultCycle: 0,
+      };
+  },
+  methods:{
+      startSearchFunction(){
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&language=it-IT&query=' + this.textUserInput)
+        .then((item) =>{
+            this.arrResponse = item.data.results; /* + [this.resultCycle] */
+            console.log(this.arrResponse);
+        })
+      }
+  }
 }
 </script>
 
--- src/components/HeaderFlix.vue
+++ src/components/HeaderFlix.vue
@@ -4,45 +4,31 @@
             <h1>BOOLFLIX</h1>
         </div>
         <div>
-            <input type="text" 
-            placeholder="Inserisci il nome:" 
-            name="searchInput" 
-            id="searchInput" 
-            v-model="textUserInput">
-            <button @click="startSearchFunction">
-                Cerca
-            </button>
+            <form @submit.prevent="$emit('searchTitle', textUserInput)">
+                <label for="searchInput">
+                    <input type="text" 
+                    placeholder="Inserisci il nome:" 
+                    name="searchInput" 
+                    id="searchInput" 
+                    v-model="textUserInput">
+                    <button>
+                        Cerca
+                    </button>
+                </label>
+            </form>
         </div>
     </header>
 </template>
 
 <script>
-import axios from 'axios';
 
 export default {
     name:'HeaderFlix',
     data(){
         return{
             textUserInput: '',
-            arrResponse: [],
-            resultCycle: 0,
         };
     },
-    /* computed(){
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&language=it-IT&query=' + this.textUserInput)
-        .then(() => {
-            
-        })
-    }, */
-    methods:{
-        startSearchFunction(){
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=2a1eafb77e5173892c5f55c2d7d7a8c8&language=it-IT&query=' + this.textUserInput)
-            .then((item) =>{
-                this.arrResponse = item.data.results[this.resultCycle];
-                console.log(this.arrResponse);
-            })
-        }
-    }
 }
 </script>
 
