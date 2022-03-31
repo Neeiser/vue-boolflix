52cfdde2338c47a317999e09fc0cacd1a2f8a1a5
Neeiser <stortini.study@gmail.com>
2022-03-31 12:04:32 +0200

    hover function

--- src/components/CardsFlix.vue
+++ src/components/CardsFlix.vue
@@ -1,16 +1,20 @@
 <template>
     <div class="card">
-        <img id="imgPoster" :src="imgPath" alt="">
-        <!-- <img :src="backupImgPath" alt=""> --> <!-- Mi serve per dopo -->
-        <h2>{{filmElement.title}}</h2>
-        <h3>{{filmElement.original_title}}</h3>
+        <div class="film-poster">
+            <img id="imgPoster" :src="imgPath" alt="">
+        </div>
+        <div class="film-info">
+            <h2>{{filmElement.title}}</h2>
+            <h3>{{filmElement.original_title}}</h3>
+            <small>{{filmElement.vote_average}}</small>
+        </div>
         <img id="flagPoster"
         v-if="this.filmElement.original_language === 'en'"
         :src='langFlagFix'>
         <img id="flagPoster"
         v-else
         :src='this.langFlag'>
-        <small>{{filmElement.vote_average}}</small>
+        <!-- <img :src="backupImgPath" alt=""> --> <!-- Mi serve per dopo -->
     </div>
 </template>
 
@@ -32,18 +36,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/partials/variables';
 
 .card{
-    background-color: lightblue;
-    border: 1px solid black;
+    border: 1px solid $blackFlix;
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: 600px;
+    height: 450px;
+    width: 300px;
+    margin: 5px;
+    position: relative;
+    .film-poster{
+    height: 450px;
     width: 300px;
     position: relative;
-    #imgPoster{
-        width: 200px;
+        #imgPoster{
+            width: 200px;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            z-index: -1;
+        }
+    }
+    .film-info{
+        display: none;
+        h3{
+            font-size: 0.8rem;
+        }
     }
     #flagPoster{
         width: 50px;
@@ -52,5 +72,21 @@ export default {
         left:0;
     }
 }
+
+.card:hover{
+    #imgPoster{
+        height: 0px;
+        transition: 0.3s;
+    }
+    .film-info{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 450px;
+        background-color: white;
+    }
+}
+
+
 
 </style>
