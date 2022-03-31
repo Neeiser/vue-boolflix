09677cb1234d4934da8531b549ca518fee9d0d98
Neeiser <stortini.study@gmail.com>
2022-03-31 10:40:42 +0200

    header style

--- src/components/HeaderFlix.vue
+++ src/components/HeaderFlix.vue
@@ -30,8 +30,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/partials/variables';
+
 header{
-    background-color: lightgray;
+    background-color: $blackFlix;
+    display: flex;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+    padding: 0px 20px;
+    h1{
+        color: $redFlix;
+    }
 }
 </style>
 
